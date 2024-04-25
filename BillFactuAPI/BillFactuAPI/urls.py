3b3020e92b751f649311730e2a350055a6483f74
adridsz <145338672+adridsz@users.noreply.github.com>
2024-04-25 20:56:06 +0200

    Implementacion models completada y terminada

--- BillFactuAPI/BillFactuAPI/urls.py
+++ BillFactuAPI/BillFactuAPI/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+#Este codigo es para poder servir los archivos en django con el modo debug activado en models, si esto se cambia dejaremos de poder mostrar los archivos
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
